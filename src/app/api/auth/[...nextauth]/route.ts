e9edcb7ee6229c75ff2b99bfc6ce1ffb729f694e
kushal bhargava <kushal.git01@gmail.com>
2024-05-01 03:21:19 +0530

    next auth route fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "./options";
-import { NextAuthOptions } from "next-auth";
 
-const handler: NextAuthOptions = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
