bfdb54da40e69694dfad2e59fb83fbdbf52c779a
kushal bhargava <kushal.git01@gmail.com>
2024-05-02 01:03:12 +0530

    Vercel upload error fixed 4

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import NextAuth from "next-auth";
 import { authOptions } from "./options";
 import { NextApiHandler } from "next";
 
-const handler: NextApiHandler = NextAuth(authOptions);
+const handler: NextApiHandler = NextAuth(authOptions) as NextApiHandler;
 
 export { handler as GET, handler as POST };
